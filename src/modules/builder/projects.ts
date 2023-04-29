5ebea00077e768dec1f137d1ad4471dbcd14627b
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-04-29 15:01:25 -0400

    Sort builder projects by update date

--- src/modules/builder/projects.ts
+++ src/modules/builder/projects.ts
@@ -3,7 +3,8 @@ import { command, fetchSpace, getInstanceMap } from "../../utils";
 import type { Instance, Project } from "../../types";
 
 export async function projects(): Promise<List> {
-  const { apps: projects } = await fetchSpace<{ apps: Project[] }>("apps");
+  const data = await fetchSpace<{ apps: Project[] }>("apps");
+  const projects = data.apps.sort((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime());
   const instanceMap = await getInstanceMap();
 
   return {
